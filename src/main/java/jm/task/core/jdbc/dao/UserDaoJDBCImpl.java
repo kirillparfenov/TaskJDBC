f3a130f84e3fd898af808de641319763e6180793
Krish <toonart.ru@gmail.com>
2021-03-16 07:50:34 +0300

    JDBC - добавил транзакции

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,6 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -17,7 +18,10 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
+        Connection connection = null;
         try {
+            connection = Util.getConnection();
+            connection.setAutoCommit(false);
             statement = Util.getConnection().createStatement();
             statement.execute("CREATE TABLE `coretaskdb`.`users` (" +
                     " `id` BIGINT(20) NOT NULL AUTO_INCREMENT," +
@@ -27,57 +31,137 @@ public class UserDaoJDBCImpl implements UserDao {
                     " PRIMARY KEY (`id`))" +
                     " ENGINE = InnoDB" +
                     " DEFAULT CHARACTER SET = utf8;");
-        } catch (SQLException ex){
+            connection.commit();
+        } catch (SQLException ex) {
+            try {
+                connection.rollback();
+            } catch (SQLException exception) {
+            }
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+                connection.close();
+            } catch (SQLException exception) {
+            }
         }
     }
 
     public void dropUsersTable() {
+        Connection connection = null;
         try {
+            connection = Util.getConnection();
+            connection.setAutoCommit(false);
             statement = Util.getConnection().createStatement();
             statement.execute("DROP TABLE users");
-        } catch (SQLException ex){
+            connection.commit();
+        } catch (SQLException ex) {
+            try {
+                connection.rollback();
+            } catch (SQLException exception) {
+            }
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+                connection.close();
+            } catch (SQLException exception) {
+            }
         }
-
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        Connection connection = null;
         try {
+            connection = Util.getConnection();
+            connection.setAutoCommit(false);
             statement = Util.getConnection().createStatement();
             statement.execute("INSERT INTO users(name, lastname, age) VALUES ('" + name + "'," +
                     "'" + lastName + "'," +
                     "'" + age + "')");
             System.out.println("User с именем – " + name + " добавлен в базу данных");
-        } catch (SQLException ex){
+            connection.commit();
+        } catch (SQLException ex) {
+            try {
+                connection.rollback();
+            } catch (SQLException exception) {
+            }
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+                connection.close();
+            } catch (SQLException exception) {
+            }
         }
     }
 
     public void removeUserById(long id) {
+        Connection connection = null;
         try {
+            connection = Util.getConnection();
+            connection.setAutoCommit(false);
             statement = Util.getConnection().createStatement();
             statement.execute("DELETE FROM users WHERE id = " + id);
-        } catch (SQLException ex){
+            connection.commit();
+        } catch (SQLException ex) {
+            try {
+                connection.rollback();
+            } catch (SQLException exception) {
+            }
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+                connection.close();
+            } catch (SQLException exception) {
+            }
         }
     }
 
     public List<User> getAllUsers() {
         List<User> list = new ArrayList<>();
         ResultSet res;
+        Connection connection = null;
         try {
+            connection = Util.getConnection();
+            connection.setAutoCommit(false);
             statement = Util.getConnection().createStatement();
             res = statement.executeQuery("SELECT * FROM users");
             while (res.next()) {
                 list.add(new User(res.getString(2), res.getString(3), res.getByte(4)));
             }
-        } catch (SQLException ex){
+            connection.commit();
+        } catch (SQLException ex) {
+            try {
+                connection.rollback();
+            } catch (SQLException exception) {
+            }
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+                connection.close();
+            } catch (SQLException exception) {
+            }
         }
         return list;
     }
 
     public void cleanUsersTable() {
+        Connection connection = null;
         try {
+            connection = Util.getConnection();
+            connection.setAutoCommit(false);
             statement = Util.getConnection().createStatement();
             statement.execute("TRUNCATE TABLE users");
-        } catch (SQLException ex){
+            connection.commit();
+        } catch (SQLException ex) {
+            try {
+                connection.rollback();
+            } catch (SQLException exception) {
+            }
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+                connection.close();
+            } catch (SQLException exception) {
+            }
         }
     }
 }
